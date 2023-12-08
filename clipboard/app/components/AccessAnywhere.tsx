cf90c70d4a27227f8d5060f0ade516ae5a3c55f4
Adefioye <abdulhakeemadefioye@gmail.com>
2023-12-08 15:19:56 -0600

    Finish features section and Started access anywhere section

--- clipboard/app/components/AccessAnywhere.tsx
+++ clipboard/app/components/AccessAnywhere.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
 
 const AccessAnywhere = () => {
   return (
-    <div>AccessAnywhere</div>
-  )
-}
+    <section id="access-anywhere">
+      <h1>Access Clipboard Anywhere</h1>
+      <p>
+        Whether you’re on the go, or at your computer, you can access all your
+        Clipboard snippets in a few simple clicks.
+      </p>
+    </section>
+  );
+};
 
-export default AccessAnywhere
+export default AccessAnywhere;
--- clipboard/app/components/Functionalities.tsx
+++ clipboard/app/components/Functionalities.tsx
@@ -1,7 +1,45 @@
+/* eslint-disable @next/next/no-img-element */
 import React from "react";
 
 const Functionalities = () => {
-  return <div>Functionalities</div>;
+  return (
+    <section id="features" className="w-full mb-16 md:mb-32">
+      <div className="section-container">
+        <div className="relative flex flex-col md:flex-row md:space-x-13">
+          <div className="md:w-1/2">
+            <img
+              src="/images/image-computer.png"
+              alt=""
+              className="md:absolute md:top-0 md:right-[50%]"
+            />
+          </div>
+
+          <div className="flex flex-col mt-16 space-y-12 text-xl md:w-1/2 md:text-left md:pl-16">
+            <div className="flex flex-col">
+              <h5>Quick Search</h5>
+              <p className="max-w-md text-grayishBlue leading-9">
+                Easily search your snippets by content, category, web address,
+                application, and more.
+              </p>
+            </div>
+            <div className="flex flex-col">
+              <h5>iCloud Sync</h5>
+              <p className="max-w-md text-grayishBlue leading-9">
+                Instantly saves and syncs snippets across all your devices.
+              </p>
+            </div>
+            <div className="flex flex-col">
+              <h5>Completely History</h5>
+              <p className="max-w-md text-grayishBlue leading-9">
+                Retrieve any snippets from the first moment you started using
+                the app.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Functionalities;
