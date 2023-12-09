9a662a9a65e6d841903f34bc8664c0fa12394d7e
Adefioye <abdulhakeemadefioye@gmail.com>
2023-12-08 20:36:24 -0600

    Finish references section

--- clipboard/app/components/References.tsx
+++ clipboard/app/components/References.tsx
@@ -1,7 +1,31 @@
+import Image from "next/image";
 import React from "react";
 
 const References = () => {
-  return <div>References</div>;
+  const IMAGES = [
+    "/images/logo-google.png",
+    "/images/logo-ibm.png",
+    "/images/logo-microsoft.png",
+    "/images/logo-hp.png",
+    "/images/logo-vector-graphics.png",
+  ];
+  return (
+    <section id="references" className="section-container my-20">
+      <div className="flex flex-col space-y-8 md:flex-row md:space-y-0 md:space-x-8">
+        {IMAGES.map((imageUrl, i) => (
+          <Image
+            key={i}
+            height={0}
+            width={0}
+            alt=""
+            src={`${imageUrl}`}
+            className="w-52 h-auto"
+            unoptimized
+          />
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default References;
